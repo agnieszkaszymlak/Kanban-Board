dc226e9a1a712dfe1720e7002f3990ff3bb9dd4d
Agnieszka Szymlak <agniepiech@gmail.com>
2019-08-19 15:36:05 +0200

    Add files .css .js .html package.json

---
+++ js/App.js
@@ -0,0 +1,49 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '3387',
+    'X-Auth-Token': '47c2fae46c8d08dfd44489d87f2da21a'
+};
+
+function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+
+    return element;
+}
+
+fetch(baseUrl + '/board', {
+        headers: myHeaders
+    })
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (resp) {
+        setupColumns(resp.columns);
+    });
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name);
+        col.addCard(cardObj);
+    });
+}
+
+
+
+
+
+
+
+
+
